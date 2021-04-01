d9a1019766f9bc1f725e3beb71464531054b2410
Daniel Kec <daniel@kecovi.cz>
2021-04-01 17:18:01 +0200

    LRAStatus body reader

--- lra/clients/rest-client/src/main/java/io/helidon/lra/rest/LRAClient.java
+++ lra/clients/rest-client/src/main/java/io/helidon/lra/rest/LRAClient.java
@@ -270,10 +270,11 @@ public class LRAClient implements Closeable {
                     name.equals(PUT.class.getName()) ||
                     name.equals(POST.class.getName()) ||
                     name.equals(DELETE.class.getName())) {
-                String pathValue = pathAnnotation.value();
-                pathValue = pathValue.startsWith("/") ? pathValue : "/" + pathValue;
-                String url = String.format("%s%s?%s=%s", uriPrefix, pathValue, LRAConstants.HTTP_METHOD_NAME, name);
-                paths.put(rel, url);
+                
+                paths.put(rel, UriBuilder.fromPath(uriPrefix)
+                        .path(pathAnnotation.value())
+                        .queryParam(HTTP_METHOD_NAME, name)
+                        .build().toASCIIString());
                 break;
             }
         }
---
+++ lra/clients/rest-client/src/main/java/io/helidon/lra/rest/LRAStatusBodyReader.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.helidon.lra.rest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+
+import org.eclipse.microprofile.lra.annotation.LRAStatus;
+
+public class LRAStatusBodyReader implements MessageBodyReader<LRAStatus> {
+
+    @Override
+    public boolean isReadable(Class<?> type,
+                              Type genericType,
+                              Annotation[] annotations,
+                              MediaType mediaType) {
+        return LRAStatus.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public LRAStatus readFrom(Class<LRAStatus> type,
+                              Type genericType,
+                              Annotation[] annotations,
+                              MediaType mediaType,
+                              MultivaluedMap<String, String> httpHeaders,
+                              InputStream entityStream) throws IOException, WebApplicationException {
+
+        String textBody = new BufferedReader(new InputStreamReader(entityStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        return LRAStatus.valueOf(textBody);
+    }
+}
